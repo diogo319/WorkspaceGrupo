e3e1c36fa8c9c4fb7baa9b9bf22540a7754e9eb8
diogo319 <diogo.mmill@gmail.com>
2016-11-11 18:06:23 +0000

    Revert "Trabalho 2 (versão final)"
    This reverts commit 07b3686308362d974e8e061af2f06713561afc82.

--- Trabalho2_com_gestor_final/src/Mensagem.java
+++
@@ -1,9 +0,0 @@
-public enum Mensagem {
-	GUI,EVITAR,VAGUEAR,EVITAR_TERMINADO,VAGUEAR_TERMINADO,GESTOR_TERMINADO,TERMINAR;
-	
-	@Override
-    public String toString() {
-        return "Comunicação: " + this.name();
-    }
-};
-
